57f260dae13960017a8cb2f1a051ccfa41ab996d
RamiMerlo <ramimerlo31@gmail.com>
2023-08-25 21:23:07 -0300

    manipulacion del DOM

--- tours.js
+++ tours.js
@@ -1,3 +1,6 @@
-// let nombre, edad
-// nombre = prompt("Como te llamas?")
-// alert("Entendido, " + nombre)
+let nombre, edad
+nombre = prompt("Como te llamas?")
+
+let miH5 = document.querySelector("#sugerenciaTicket")
+
+miH5.textContent = `${nombre}, te recomendamos estas fechas`
