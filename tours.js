87b02fe6363ab5823034b9dee11cd6e71d2834ea
RamiMerlo <ramimerlo31@gmail.com>
2023-08-26 17:02:07 -0300

    DOM change

--- tours.js
+++ tours.js
@@ -1,6 +1,25 @@
 let nombre, edad
-nombre = prompt("Como te llamas?")
-
+let nombreValido = false
+let nombreNuevo
+do {
+  nombre = prompt("Como te llamas?")
+  if (nombre === "") {
+    nombre = String(prompt("Debe ingresar un nombre valido."))
+  } else if (nombre.length <= 2) {
+    nombre = String(prompt("Debe ingresar un nombre mas largo."))
+  } else {
+    nombreValido = true
+  }
+} while (nombreValido == false)
 let miH5 = document.querySelector("#sugerenciaTicket")
 
-miH5.textContent = `${nombre}, te recomendamos estas fechas`
+if (nombreValido == true) {
+  const primeraLetra = nombre.charAt(0)
+  const restoDelNombre = nombre.slice(1)
+
+  nombreNuevo = String(
+    primeraLetra.toUpperCase() + restoDelNombre.toLowerCase()
+  )
+}
+
+miH5.textContent = `${nombreNuevo}, te recomendamos estas fechas`
