ec10e399fc494a4fefa3f0c5e4396dc5f7b3ec36
RamiMerlo <ramimerlo31@gmail.com>
2023-09-22 04:17:55 -0300

    dasdsadsaasd

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const url =
-  "mongodb+srv://ramimerlo31:Legolass3110@ramimerlo.ekatfox.mongodb.net/?retryWrites=true&w=majority"
-const routes = require("./routes/index")
+let nav = document.querySelector("#nav")
+let navImg = document.querySelector("#navImage")
+let items = document.querySelectorAll(".navItems")
+let usuarioFront = document.querySelector("#nickName")
 
-app.use(express.json())
-app.use("/", routes)
+// const express = require("express")
+// const app = express()
+// const mongoose = require("mongoose")
+// const url =
+//   "mongodb+srv://ramimerlo31:Legolass3110@ramimerlo.ekatfox.mongodb.net/?retryWrites=true&w=majority"
+// const routes = require("./routes/index")
 
-const connectMongo = async () => {
-  try {
-    await mongoose.connect(url)
-    app.listen(3000, () => {
-      console.log("escuchando en el puerto 3000 y la base de datos conectada")
-    })
-  } catch (error) {
-    console.log(error)
-  }
-}
+// app.use(express.json())
+// app.use("/", routes)
 
-connectMongo()
+// const connectMongo = async () => {
+//   try {
+//     await mongoose.connect(url)
+//     app.listen(3000, () => {
+//       console.log("escuchando en el puerto 3000 y la base de datos conectada")
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// connectMongo()
+navImg.addEventListener("click", () => {
+  nav.classList.toggle("nav-opacity")
+  items.forEach((item) => {
+    item.classList.toggle("nav-items-toggled")
+  })
+})
--- models/users.js
+++ models/users.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose")
-const Usuario = new mongoose.Schema({
-  nombre: { type: String },
-  tipo: { type: String },
-  stock: { type: Number, min: [0, "no puede ser negativo"] },
-  precio: { type: Number },
-})
+// const mongoose = require("mongoose")
+// const Usuario = new mongoose.Schema({
+//   nombre: { type: String },
+//   tipo: { type: String },
+//   stock: { type: Number, min: [0, "no puede ser negativo"] },
+//   precio: { type: Number },
+// })
 
-module.exports = mongoose.model("Usuario", Usuario)
+// module.exports = mongoose.model("Usuario", Usuario)
